409f20f7f4b5285ba8b198de7ab6157826e9d9fa
nuxarch <misbaqul4sfi4k>
2022-08-09 18:22:04 +0700

    adding-ntp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,3 @@
-// REQUIRES the following Arduino libraries:
-// - Adafruit RGB matrix Panel Library: https://github.com/adafruit/RGB-matrix-Panel
-// - Adafruit_GFX Library: https://github.com/adafruit/Adafruit-GFX-Library
-// - Adafruit_BusIO Library: https://github.com/adafruit/Adafruit_BusIO
-// - package_esp32_index.json https://dl.espressif.com/dl/package_esp32_index.json
-// Find All "Great Projects" Videos : https://www.youtube.com/channel/UCCC8DuqicBtP3A_aC53HYDQ/videos
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Adafruit_GFX.h>
@@ -11,7 +5,7 @@
 
 #include "Fonts/FreeMonoBold12pt7b.h"
 #include "Fonts/kongtext4pt7b.h"
-#define TZ 1 // Time Zone
+#define TZ 7 // Time Zone
 #define CLK 14
 #define OE 33
 #define LAT 32
@@ -20,10 +14,11 @@
 #define C 4  //17
 #define D 18
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 32);
-String Message0 = "To all people, I love you and wish you safety and recovery for patients";
-String Message1 = "Great Projects wish you a very nice day. Hope you have a great time ahead.";
+// String Message0 = "To all people, I love you and wish you safety and recovery for patients";
+String Message1 = "";
+// String Message1 = "Politeknik Negeri Malang";
 bool flasher = false;
-uint8_t frameDelay = 25; // default frame delay value
+uint8_t frameDelay = 80; // default frame delay value
 static time_t last_t;
 uint8_t r = 0, g = 0, b = 0;
 int h, m, s, d, yr;
@@ -33,10 +28,12 @@ unsigned int NewRTCh = 24;
 unsigned int NewRTCm = 60;
 unsigned int NewRTCs = 10;
 time_t t;
-char ssid[] = "Nux Arch";
-char pass[] = "akhtar2015";
-const char *wd[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+// char ssid[] = "Nux Arch";
+// char pass[] = "akhtar2015";
+char ssid[] = "Ravensburg";
+char pass[] = "4khtar2015";
+const char *wd[7] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"};
+const char *months[] = {"Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Ags", "Sep", "Okt", "Nop", "Des"};
 
 // standard colors
 uint16_t myRED = matrix.Color333(7, 0, 0);
@@ -54,8 +51,8 @@ void getTim()
 {
   if (flasher)
   {
-    matrix.fillRect(54, 10, 10, 6, myBLACK);
-    matrix.setCursor(20, 8);
+    matrix.fillRect(54, 10, 10, 6, myWHITE);
+    matrix.setCursor(10, 17);
     matrix.setTextSize(2);
     matrix.setTextColor(myCOLORS[g]);
     matrix.print(":");
@@ -63,7 +60,7 @@ void getTim()
   }
   else
   {
-    matrix.fillRect(24, 12, 2, 6, myBLACK);
+    // matrix.fillRect(24, 12, 2, 6, myRED);
     matrix.setCursor(54, 10);
     matrix.setTextSize(1);
     matrix.setFont(&kongtext4pt7b);
@@ -74,12 +71,12 @@ void getTim()
   }
   if (NewRTCs != s / 10)
   {
-    matrix.fillRect(51, 17, 13, 6, myBLACK);
+    matrix.fillRect(51, 17, 13, 6, myBLUE);
     NewRTCs = s / 10;
   }
   else
   {
-    matrix.fillRect(58, 17, 6, 6, myBLACK);
+    matrix.fillRect(58, 17, 6, 6, myGREEN);
   }
   matrix.setCursor(51, 16);
   matrix.setTextSize(1);
@@ -89,10 +86,11 @@ void getTim()
   matrix.setFont();
   if (NewRTCm != m)
   {
-    matrix.setCursor(26, 16);
-    matrix.setFont(&FreeMonoBold12pt7b);
+    matrix.setCursor(17, 16);
+    // matrix.setFont(&FreeMonoBold12pt7b);
+    matrix.setFont(&kongtext4pt7b);
     matrix.setTextColor(myMAGENTA);
-    matrix.fillRect(26, 8, 25, 15, myBLACK);
+    matrix.fillRect(17, 16, 13, 8, myBLACK);
     matrix.printf("%02d", m);
     matrix.setFont();
     NewRTCm = m;
@@ -100,26 +98,33 @@ void getTim()
   if (NewRTCh != h)
   {
     matrix.setCursor(0, 16);
-    matrix.setFont(&FreeMonoBold12pt7b);
+    // matrix.setFont(&FreeMonoBold12pt7b);
+    matrix.setFont(&kongtext4pt7b);
     matrix.setTextColor(myMAGENTA);
-    matrix.fillRect(0, 8, 24, 15, myBLACK);
+    matrix.fillRect(0, 17, 13, 8, myBLACK);
     matrix.printf("%02d", h);
     matrix.setFont();
     NewRTCh = h;
   }
 }
-void Date_text()
+// void Date_text()
+// {
+//   matrix.setCursor(1, 23);
+//   matrix.fillRect(0, 23, 64, 8, myBLACK);
+//   matrix.setFont(&kongtext4pt7b);
+//   matrix.setTextColor(myRED);
+//   matrix.printf("%02d", d);
+//   matrix.setTextColor(myGREEN);
+//   matrix.printf("%s", months[month]);
+//   matrix.setTextColor(myRED);
+//   matrix.printf("%04d", yr);
+//   matrix.setFont();
+// }
+void Date_get()
 {
-  matrix.setCursor(1, 23);
-  matrix.fillRect(0, 23, 64, 8, myBLACK);
-  matrix.setFont(&kongtext4pt7b);
-  matrix.setTextColor(myRED);
-  matrix.printf("%02d", d);
-  matrix.setTextColor(myGREEN);
-  matrix.printf("%s", months[month]);
-  matrix.setTextColor(myRED);
-  matrix.printf("%04d", yr);
-  matrix.setFont();
+  
+  Serial.println("["+String(wd[dow])+", "+String(d)+" "+String(months[month])+" "+String(yr)+"]");
+  Message1 = ""+String(wd[dow])+", "+String(d)+" "+String(months[month])+" "+String(yr)+" PRODI TPPU";
 }
 void dofw_text()
 {
@@ -183,11 +188,11 @@ void scroll_text1(uint8_t ypos, unsigned long scroll_delay, String text)
 {
   uint16_t text_length = text.length();
   // Asuming 5 pixel average character width
-  for (int xpos = 64; xpos > -(64 + text_length * 7); xpos--)
+  for (int xpos = 32; xpos > -(32 + text_length * 7); xpos--)
   {
     int y = r;
     matrix.setCursor(xpos, ypos);
-    matrix.fillRect(0, ypos, 64, 8, myBLACK);
+    matrix.fillRect(0, ypos, 32, 8, myBLACK);
     matrix.setFont(&kongtext4pt7b);
     for (int x = 0; x < text_length; x++)
     {
@@ -219,40 +224,29 @@ void setup()
   Serial.print("Attempting to connect to Network named: ");
   Serial.println(ssid); // print the network name (SSID);
 
-  // WiFi.begin(ssid, pass);
-  // while (WiFi.status() != WL_CONNECTED)
-  // {
-  //   Serial.print(".");
-  //   delay(1000);
-  // }
-  // Serial.println("");
-  // IPAddress ip = WiFi.localIP();
-  // Serial.print("IP Address: ");
-  // Serial.println(ip);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 
-  // configTime(TZ * 3600, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp"); // enable NTP
+  configTime(TZ * 3600, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp"); // enable NTP
 
   matrix.begin();            // setup the LED matrix
   matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
   matrix.fillScreen(0);
+
 }
 
 void loop()
 {
-  matrix.setTextSize(1);
-  // Date_text();
+  updateTime();
+  getTim();
+  Date_get();
   scroll_text1(0, frameDelay, Message1);
-  // dofw_text();
-  // scroll_text1(23, frameDelay, Message0);
-  // Date_text();
-  // matrix.setTextSize(1);
-  // scroll_text(13, 15, Message0);
-  // matrix.setTextSize(1);
-  // dofw_text();
-  // scroll_text1(23, frameDelay, Message0);
-  // Date_text();
-  // scroll_text1(0, frameDelay, Message1);
-  // dofw_text();
-  // matrix.setTextSize(2);
-  // scroll_text(13, 15, Message1);
 }
